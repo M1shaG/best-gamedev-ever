f744ac2febffbe8b2a875f2b54e17d5342021f4c
M1shaG <mg834247@gmail.com>
2025-07-27 19:54:14 +0300

    ya eblan

--- scripts/main_ch.gd
+++ scripts/main_ch.gd
@@ -7,21 +7,11 @@ func _ready():
 	main_ch = $Sprite2D
 func get_input():
 	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	
-	if input_direction.x != 0:
-		if input_direction.x > 0:
-			main_ch.rotation_degrees = 180
-		if input_direction.x < 0:
-			main_ch.rotation_degrees = 0
-			
-	if input_direction.y < 0:
-		main_ch.rotation_degrees = 90
-	if input_direction.y > 0:
-		main_ch.rotation_degrees = 270
-	
 	velocity = input_direction * speed
 	
-	
+	if input_direction != Vector2.ZERO:
+		main_ch.rotation = input_direction.angle() + PI
+
 
 func _physics_process(delta):
 	get_input()

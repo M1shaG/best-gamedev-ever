1f32f0d0d1dcbc57e470aca0934bd867af77114c
M1shaG <mg834247@gmail.com>
2025-07-27 19:49:46 +0300

    add rotation to sprite(komu ne poxuy lol)

--- scripts/main_ch.gd
+++ scripts/main_ch.gd
@@ -1,10 +1,27 @@
 extends CharacterBody2D
 
 @export var speed = 400
+var main_ch: Sprite2D
 
+func _ready():
+	main_ch = $Sprite2D
 func get_input():
 	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	if input_direction.x != 0:
+		if input_direction.x > 0:
+			main_ch.rotation_degrees = 180
+		if input_direction.x < 0:
+			main_ch.rotation_degrees = 0
+			
+	if input_direction.y < 0:
+		main_ch.rotation_degrees = 90
+	if input_direction.y > 0:
+		main_ch.rotation_degrees = 270
+	
 	velocity = input_direction * speed
+	
+	
 
 func _physics_process(delta):
 	get_input()
